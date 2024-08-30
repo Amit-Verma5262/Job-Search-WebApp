de812dc931b8c3396828c82a7fa1fd24946b1a61
Amit-Verma5262 <amitverma5262.02@gmail.com>
2024-08-30 19:09:59 +0530

    added frontend

--- backend/index.js
+++ backend/index.js
@@ -12,35 +12,33 @@ dotenv.config({});
 
 const app = express();
 
-
-// middleware
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Updated CORS configuration
 const corsOptions = {
-    origin: '*',
-    credentials: true
-  };  
+    origin: 'https://jobsearchui-git-main-amit-verma-s-projects.vercel.app', // Set to your frontend's exact URL
+    credentials: true // Allow credentials
+};
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Preflight requests handling
 
 const PORT = process.env.PORT || 3000;
 
-
-// api's
+// API routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.get("/",(req,res)=>{
-  res.send("Hello User")
-})
+app.get("/", (req, res) => {
+    res.send("Hello User");
+});
 
-
-
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
