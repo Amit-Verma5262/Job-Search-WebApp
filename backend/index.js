7afaf1636f582c3147595ffab850f0c1fd154805
Amit-Verma5262 <amitverma5262.02@gmail.com>
2024-08-30 18:09:32 +0530

    Minor fix

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ dotenv.config({});
 
 const app = express();
 
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -31,6 +32,11 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+
+app.get("/",(req,res)=>{
+  res.send("Hello User")
+})
+
 
 
 
