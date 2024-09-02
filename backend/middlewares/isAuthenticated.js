72dbf809b3082e57532096337eced30fafff1873
Amit-Verma5262 <amitverma5262.02@gmail.com>
2024-09-02 10:20:10 +0530

    changes isauthenticated

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -2,21 +2,21 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.cookies.token;
+        const {token} = req.cookies;
         if (!token) {
             return res.status(401).json({
                 message: "User not authenticated",
                 success: false,
             })
         }
-        const decode = await jwt.verify(token, process.env.SECRET_KEY);
-        if(!decode){
-            return res.status(401).json({
-                message:"Invalid token",
-                success:false
-            })
-        };
-        req.id = decode.userId;
+        const decode =  jwt.verify(token, process.env.SECRET_KEY);
+        // if(!decode){
+        //     return res.status(401).json({
+        //         message:"Invalid token",
+        //         success:false
+        //     })
+        // };
+        req.user=await User.findById(decode.id)
         next();
     } catch (error) {
         console.log(error);
